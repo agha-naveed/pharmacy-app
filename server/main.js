f6e55cb6b4a100e3b097cf2c26ad32c8f3e18d02
agha-naveed <naveedabs31@gmail.com>
2025-03-04 11:40:59 +0500

    added backend

---
+++ server/main.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const app = express()
+const router = express.Router()
+
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/ali_pharmacy_skd")
+.then(() => {
+    console.log("Server Connected...")
+}).catch(() => console.log("Error while connecting to the server..."))
+
+
+// router.route('/')
+// .get()
+
+
+
+
+
+app.listen(8000, () => {
+    console.log("server running on port: "+ 8000)
+})
