462aa779d44b7aa21567d71a33b25848914a8dd0
agha-naveed <naveedabs31@gmail.com>
2025-03-12 13:18:37 +0500

    adding search in payment status

--- src/components/PendingPayments.tsx
+++ src/components/PendingPayments.tsx
@@ -1,10 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Header from '../extra-components/Header'
+import { LuSearch } from "react-icons/lu";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 export default function PendingPayments() {
     const [detail, setDetail] = useState<any>([])
     const [id, setId] = useState<any>("")
     const [edit, setEdit] = useState<any>()
+    const [focus, setFocus] = useState<boolean>(false)
+    const [searchQuery, setSearchQuery] = useState<any>("")
+    const searchRef = useRef<any>(null)
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         async function fetchData() {
@@ -38,10 +46,67 @@ export default function PendingPayments() {
         updateData()
     }, [id])
 
+    useEffect(() => {
+        if(searchQuery.length > 0) {
+            
+        }
+    }, [searchQuery])
     return (
         <div className='w-full overflow-hidden'>
             <Header value="Payment Status" />
             <div className='!p-1'>
+
+                <div className='flex justify-between p-sec'>
+                    <div className='flex items-center' 
+                    onFocus={() => setFocus(true)}
+                    onBlur={() => setFocus(false)}
+                    >
+                        <input
+                        type="text"
+                        className={`
+                        border
+                        border-black
+                        outline-orange-500
+                        h-10
+                        !pl-2
+                        !pr-9
+                        rounded-lg
+                        transition-all
+                        ${focus ? "w-96" : 'w-52'}
+                        `}
+                        ref={searchRef}
+                        onInput={(e:any) => searchQuery(e.target.value)}
+                        title='Search Bar'
+                        placeholder='Click to Search...'
+                        />
+                        <button className={`
+                            text-[22px]
+                            !py-2
+                            !px-3
+                            relative
+                            right-11
+                            rounded-r-lg
+                            cursor-pointer
+                            text-white border
+                            ${focus ? "border-orange-500" : "border-black"}
+                            bg-orange-500
+                            hover:bg-orange-600
+                            transition-all
+                            `} title='Search'
+                            onClick={() => searchRef.current ? searchQuery(searchRef.current.value) : ""}
+                            >
+                            <LuSearch />
+                        </button>
+                    </div>
+                    
+                    <button
+                    onClick={() => navigate("/medicine-panel/new-medicine")}
+                    className='flex gap-2 cursor-pointer hover:bg-slate-900 transition-all items-center bg-slate-800 text-white rounded-full w-fit !px-7 !py-[10px] self-end'>
+                    <FaRegPlusSquare className='text-xl' />
+                    <span>Add New Entry</span>
+                    </button>
+                </div>
+                
                 <section className='w-full overflow-x-auto !px-2 !pb-1 !mt-7'>
                     <div className="w-fit">
                         <table className="table">
