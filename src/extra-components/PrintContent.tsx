8717664d725dc42d87421e957e486b55c81f340d
agha-naveed <naveedabs31@gmail.com>
2025-03-20 13:48:44 +0500

    ...

--- src/extra-components/PrintContent.tsx
+++ src/extra-components/PrintContent.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { useReactToPrint } from "react-to-print";
 import { useParams } from 'react-router-dom';
-import logo from '../assets/img/logo.png'
+import logo from '../assets/logo.png'
 import sign from '../assets/img/Narf_signature.png'
 import qr from '../assets/img/qr-code.png'
 
