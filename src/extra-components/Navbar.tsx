9dc0afb3e6064beeb303d7ccf06b0dc72fd13a5a
Syed Naveed Abbas <naveedabs31@gmail.com>
2025-05-22 20:58:07 +0500

    .

--- src/extra-components/Navbar.tsx
+++ src/extra-components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
     return (
         <div className='bg-slate-700 min-h-screen !w-[360px] !py-5'>
             <Link to={"/"} className='w-fit flex justify-self-center'>
-                <img src={logo} className='spin w-24 select-none' alt='Logo' />
+                <img src={logo} className='w-24 select-none' alt='Logo' />
             </Link>
             <span className='text-center w-full block text-2xl !py-[5px] text-white font-bold select-none'>Agha Pharmacy</span>
 
